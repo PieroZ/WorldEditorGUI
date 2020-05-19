11eefc1085485be095c22eccc03d4c413b3a0023
PieroZ <piotrotron@gmail.com>
2020-05-19 17:39:00 +0200

    Added premake config to VisualStudio

---
+++ premake5.lua
@@ -0,0 +1,39 @@
+workspace "WorldEditorGUI"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "WorldEditorGUI"
+	location "WorldEditorGUI"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		symbols "On"
+
+	filter "configurations:Release"
+		optimize "On"
+	
+	filter "configurations:Dist"
+		symbols "On"
